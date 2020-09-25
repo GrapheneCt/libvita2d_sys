718f8fbdc5b31cf5ae63f176241a56ccd23d0582
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-09-25 18:34:03 +0200

    Sample update

--- sample/src/main.c
+++ sample/src/main.c
@@ -2,48 +2,11 @@
 #include <vita2d_sys.h>
 #include <psp2/appmgr.h>
 
-#define CLIB_HEAP_SIZE 1 * 1024 * 1024
-
-extern void* sceClibMspaceCreate(void* base, uint32_t size);
-
-typedef struct sceAppMgrBudgetInfo {
-	SceSize size;
-	uint32_t mode;
-	uint32_t unk_4;
-	uint32_t budgetLPDDR2;
-	uint32_t freeLPDDR2;
-	uint32_t allow0x0E208060;
-	uint32_t unk_14;
-	uint32_t budget0x0E208060;
-	uint32_t free0x0E208060;
-	uint32_t unk_20;
-	uint32_t unk_24;
-	uint32_t budgetPHYCONT;
-	uint32_t freePHYCONT;
-	uint32_t allow;
-	char unk_34[0x20];
-	uint32_t unk_54;
-	uint32_t budgetCDRAM;
-	uint32_t freeCDRAM;
-	char reserved_60[0x24];
-} sceAppMgrBudgetInfo;
-
-int sceAppMgrGetBudgetInfo(sceAppMgrBudgetInfo* info);
-
-int main() {
-
-	void* mspace;
-	void* clibm_base;
-	SceUID clib_heap = sceKernelAllocMemBlock("ClibHeap", SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE, CLIB_HEAP_SIZE, NULL);
-	sceKernelGetMemBlockBase(clib_heap, &clibm_base);
-
-	mspace = sceClibMspaceCreate(clibm_base, CLIB_HEAP_SIZE);
+void _start(unsigned int args, void *argp) {
 	
 	int x, size;
 	x = 0;
 	size = 0;
-
-	vita2d_clib_pass_mspace(mspace);
 
    	vita2d_init();
 
@@ -55,16 +18,16 @@ int main() {
 
 	char mes1[17];
 	char mes2[19];
-	sceAppMgrBudgetInfo info = { 0 };
+	SceAppMgrBudgetInfo info = { 0 };
 	info.size = 0x88;
 	sceAppMgrGetBudgetInfo(&info);
-	sprintf(mes1, "Free LPDDR2: %d", info.freeLPDDR2 / 1024 / 1024);
-	sprintf(mes2, "LPDDR2 Budget: %d", info.budgetLPDDR2 / 1024 / 1024);
+	sprintf(mes1, "Free LPDDR2: %d MB", info.freeMain / 1024 / 1024);
+	sprintf(mes2, "LPDDR2 Budget: %d MB", info.budgetMain / 1024 / 1024);
 
     while (1) {
 
-        	vita2d_start_drawing();
-        	vita2d_clear_screen();
+        vita2d_start_drawing();
+        vita2d_clear_screen();
 
 		if (x == 544)
 			x = 0;
@@ -82,8 +45,6 @@ int main() {
 		vita2d_end_shfb();
         
 	}
-
-	return 0;
 
 }
 
